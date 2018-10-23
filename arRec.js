4d4cd66fb8c19c038f0e5235f1d69455ec5d6fcc
tomoose11 <tomwall_84@hotmail.co.uk>
2018-10-23 22:49:39 +0100

    2

--- arRec.js
+++ arRec.js
@@ -1,35 +1,67 @@
-let arr1 = [
-  "Jeff",
-  "john",
-  ["hank", ["simon"], "jamie"],
-  "matt",
-  ["ham", "cheese"]
-];
-let ind = "";
-let bool2 = false;
-function arRec(index, arr) {
+const analyzeArray = (collectionName, arr, path = "") => {
+  if (arr.length === 0) {
+    console.log(`${collectionName}.${0}:`);
+  }
+  let result = [];
+  let other = [];
   for (let i = 0; i < arr.length; i++) {
-    ind += i.toString();
-    let bool = false;
-    if (Array.isArray(arr[i])) {
-      ind += arRec(index, arr[i]).toString();
-
-      bool = true;
-      ind = ind
-        .split("")
-        .slice(2, -1)
-        .join("");
+    if (i === 0) {
+      path += i;
     }
-    if (!bool) {
-      console.log(ind, arr[i]);
-      ind = ind
-        .split("")
-        .slice(0, -1)
-        .join("");
+    if (i > 0) {
+      path = path.slice(0, -1) + i;
+    }
+    if (Array.isArray(arr[i])) {
+      result = analyzeArray(collectionName, arr[i], path);
+    } else {
+      console.log(`${collectionName}.${path}: ${arr[i]}`);
+      other.push(`${collectionName}${path}${arr[i]}`);
+      //console.log(mySpy(`${collectionName}.${path}: ${arr[i]}`));
     }
   }
+  return [...result, ...other];
+};
 
-  return ind.toString();
+analyzeArray("Frank", [
+  "Biff",
+  [["pp"], "ttt", "dd", ["gy", ["uu"], "tt"], "bb"],
+  "jan",
+  "ll"
+]);
+
+function mySpy(str) {
+  return str;
 }
 
-arRec("ff", arr1);
+// const { expect } = require("chai");
+// const { analyzeArray } = require("../analyzeArray");
+// const { assert } = require("chai");
+// const sinon = require("sinon");
+
+// describe("analyzeArray", () => {
+//   let spy = sinon.spy(console, "log");
+//   it("given an empty array should log a collection name followed by index 0", () => {
+//     analyzeArray("Bryan", []);
+//     assert(spy.calledWith("Bryan.0:"));
+//   });
+//   it("finds path for a single item in a flat array", () => {
+//     analyzeArray("Frank", ["toy"]);
+//     assert(spy.calledWith("Frank.0: toy"));
+//   });
+//   it("should work for any number of items in a flat array", () => {
+//     analyzeArray("bobby", ["a", "b", "c"]);
+//     assert(spy.calledWith("bobby.0: a"));
+//     assert(spy.calledWith("bobby.1: b"));
+//     assert(spy.calledWith("bobby.2: c"));
+//   });
+//   it("should log all items with correct indexes even when items are nested at several levels", () => {
+//     analyzeArray("bobby", ["a", "b", ["c", ["d"], "e"], "f"]);
+//     assert(spy.calledWith("bobby.0: a"));
+//     assert(spy.calledWith("bobby.1: b"));
+//     assert(spy.calledWith("bobby.20: c"));
+//     assert(spy.calledWith("bobby.210: d"));
+//     assert(spy.calledWith("bobby.22: e"));
+//     assert(spy.calledWith("bobby.3: f"));
+//     spy.restore();
+//   });
+// });
